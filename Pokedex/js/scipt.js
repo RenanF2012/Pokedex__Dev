993832903127b916f13a3d757c4ae7ba1a1f43a0
RenanF2012 <132415076+RenanF2012@users.noreply.github.com>
2024-05-14 12:29:12 -0300

    Update scipt.js

--- Pokedex/js/scipt.js
+++ Pokedex/js/scipt.js
@@ -1,3 +1,9 @@
-const fetchPokemon = () => {
-    
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    const data = await APIResponse.json();
+
+    console.log(data);
 }
+
+fetchPokemon('25');
